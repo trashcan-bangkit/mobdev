19098fc2cfe271c721fd8b7380cea674e9621582
nabilaawangga <nabila.zahra11@ui.ac.id>
2024-05-28 13:03:23 +0700

    welcome page ver 1

---
+++ app/src/main/java/com/capstone/trashcan/view/EmailEditText.kt
@@ -0,0 +1,4 @@
+package com.capstone.trashcan.view
+
+class EmailEditText {
+}
--- app/src/main/java/com/capstone/trashcan/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/capstone/trashcan/view/welcome/WelcomeActivity.kt
@@ -1,21 +1,50 @@
 package com.capstone.trashcan.view.welcome
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.trashcan.R
+import com.capstone.trashcan.databinding.ActivityWelcomeBinding
+import com.capstone.trashcan.view.login.LoginActivity
+import com.capstone.trashcan.view.signup.SignupActivity
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.signupButton.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
         }
     }
 }
